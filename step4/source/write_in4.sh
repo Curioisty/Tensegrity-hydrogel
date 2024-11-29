b657029c793333c831c412ce9b979c9da702582c
haoqi zhu <hqzhu005@gmail.com>
2024-11-29 15:15:55 +0800

    Add files via upload

---
+++ step4/source/write_in4.sh
@@ -0,0 +1,115 @@
+read   rand1 rand2 < tmp_random
+echo   variable T  equal 1.0
+
+echo   units            lj
+echo   boundary         s s s 
+echo   atom_style       angle
+echo   log              log.polymer.txt
+echo   read_data        polymer3.txt
+
+echo  mass   \*  1
+
+echo   region 6wall block  -10 110 -10 110  0 100
+echo   fix w1  all wall/region 6wall lj126 1 1 2.5
+
+echo  region  r_lower   block  0  100  0  100    0  20 
+echo  region  r_upper   block  0  100  0  100    80 100
+
+
+echo  group bm  type 2
+echo  group AAm type 1 
+echo  group alower region r_lower
+echo  group aupper region r_upper
+echo  group lower intersect bm alower
+echo  group upper intersect bm aupper
+
+echo  group boundary union upper lower
+echo  group a_center subtract  all  boundary 
+
+
+echo  bond_style   harmonic
+echo  bond_coeff   1      10      1.0
+echo  bond_coeff   2     100      1.0
+echo  bond_coeff   3     100      2.0
+
+
+echo  angle_style     harmonic
+echo  angle_coeff     1 1000.0  180.0  
+
+echo  special_bonds   lj 0.0 1.0 1.0
+echo  pair_style      lj/cut   2.2448    # 1.1224
+echo  pair_coeff        1   1   10   0.5   0.5612
+echo  pair_coeff        1   2   10   1.0   1.1224
+echo  pair_coeff        1   3  100   1.25  2.0
+echo  pair_coeff        1   4  100   1.25  2.0
+echo  pair_coeff        2   2   10   1.0   1.1224
+echo  pair_coeff        2   3   10   1.25  1.4
+echo  pair_coeff        2   4   10   1.25  1.4
+echo  pair_coeff        3   3   10   2.0   2.2448
+echo  pair_coeff        3   4   10   2.0   2.2448
+echo  pair_coeff        4   4   10   2.0   2.2448
+
+echo  neighbor 1.0 bin
+echo  neigh_modify every 1 delay 10 check yes
+echo  comm_modify mode single cutoff 4.1
+
+echo  velocity  AAm  create  0.01  ${rand1}   # 1 is temperature, seed for random
+echo  fix  1 all nve/limit 0.05 # 0.05 is max distance an atom can move in a timestep
+echo  fix duanjian AAm bond/break  100  1  3.3  prob 0.05 ${rand1}
+
+echo timestep 0.01
+echo  run 200
+echo  reset_timestep 0
+
+echo fix sf1  lower  setforce 0 0 0 
+echo fix sf2  upper  setforce 0 0 0 
+
+echo  variable  longforce equal f_sf2[3]-f_sf1[3]
+echo  fix myat2 all ave/time 10 10 100 v_longforce file forcetube.dat
+
+
+echo   fix m1 lower  move linear 0 0   0.1 units lattice
+echo   fix m2 upper  move linear 0 0  -0.1 units lattice
+
+
+echo   compute ss  all stress/atom NULL
+echo   compute vol all  voronoi/atom
+echo   variable vonpress atom sqrt\(0.5*\(\(c_ss[1]-c_ss[2]\)^2+\(c_ss[1]-c_ss[3]\)^2+\(c_ss[2]-c_ss[3]\)^2+6*\(c_ss[4]^2+c_ss[5]^2+c_ss[6]^2\)\)\)
+echo   variable stress equal -pzz*/1.37*1000000
+echo   compute newT a_center temp
+echo   compute cml lower com
+echo   compute cmr upper com
+
+echo  thermo_style custom step temp  bonds ebond pzz c_cml[3]  c_cmr[3]
+echo  thermo 1000
+echo  thermo_modify temp newT
+echo  timestep  0.01
+echo  dump 1 AAm  custom   1000    AAm3.crack id mol type  x y z  v_vonpress
+echo  dump 2 all  custom   1000 jieguo3.crack id mol type  x y z  v_vonpress  #vmd_jieguo图
+echo  dump_modify 1 sort id
+echo  dump_modify 2 sort id
+
+echo  run 5000
+echo  write_data pull1.data
+
+echo  run 5000
+echo  write_data pull2.data
+
+echo  run 5000
+echo  write_data pull3.data
+
+echo  run 5000
+echo  write_data pull4.data
+
+echo  run 5000
+echo  write_data pull5.data
+
+echo  run 5000
+echo  write_data pull6.data
+
+echo  run 5000
+echo  write_data pull7.data
+
+echo  run 3000
+echo  write_data pull8.data
+
